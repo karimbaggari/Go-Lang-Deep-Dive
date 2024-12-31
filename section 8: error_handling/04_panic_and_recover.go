91a6a076c88f571c65c19eaff8f4615cc7207be0
karim <kariim.baggarii@gmail.com>
2024-12-31 23:57:48 +0100

    add panic and recover

---
+++ section 8: error_handling/04_panic_and_recover.go
@@ -0,0 +1,23 @@
+// Panic and Recover
+
+package main
+
+import "fmt"
+
+func safeDivision(a, b int) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+		}
+	}()
+
+	if b == 0 {
+		panic("division by zero")
+	}
+	fmt.Println("Result:", a/b)
+}
+
+func main() {
+	safeDivision(10, 2)
+	safeDivision(10, 0)
+}
