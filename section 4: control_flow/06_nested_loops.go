443d8ff529ba8561426049df92885911455724f5
karim <kariim.baggarii@gmail.com>
2025-01-01 05:14:00 +0100

    update on nested loop

--- section 4: control_flow/06_nested_loops.go
+++ section 4: control_flow/06_nested_loops.go
@@ -7,6 +7,12 @@ import "fmt"
 func main() {
 	for i := 1; i <= 3; i++ {
 		for j := 1; j <= 2; j++ {
+			if j == 1 {
+				continue
+			}
+			if i == 2 {
+				break
+			}
 			fmt.Printf("Outer: %d, Inner: %d\n", i, j)
 		}
 	}
