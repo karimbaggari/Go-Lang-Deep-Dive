01d19dd66ca67f52df2f12c13b49eaa578d67f1f
karim <kariim.baggarii@gmail.com>
2025-01-02 04:19:46 +0100

    complete the channels section

--- section 13: channels/01_basic_channels.go
+++ section 13: channels/01_basic_channels.go
@@ -1,14 +1,27 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
+
+func sendMessage(ch chan string, msg string) {
+    ch <- msg // Send the message to the channel
+}
 
 func main() {
-	ch := make(chan int)
+    // Create two channels
+    ch1 := make(chan string)
+    ch2 := make(chan string)
 
-	go func() {
-		ch <- 42
-	}()
+    // Start two goroutines, each sending a message to its respective channel
+    go sendMessage(ch1, "Message from channel 1")
+    go sendMessage(ch2, "Message from channel 2")
 
-	val := <-ch
-	fmt.Println("Received:", val)
+    // Receive messages from both channels and log them
+    msg1 := <-ch1
+    fmt.Println("Received:", msg1)
+    msg2 := <-ch2
+    fmt.Println("Received:", msg2)
 }
+
+// this example will be more clear when you check the select.go file the completion is there
--- section 13: channels/02_select.go
+++ section 13: channels/02_select.go
@@ -1,23 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
+
+func sendMessage(ch chan string, msg string) {
+    ch <- msg // Send the message to the channel
+}
 
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
+    // Create two channels
+    ch1 := make(chan string)
+    ch2 := make(chan string)
 
-	go func() {
-		ch1 <- "Channel 1"
-	}()
+    // Start two goroutines, each sending a message to its respective channel
 
-	go func() {
-		ch2 <- "Channel 2"
-	}()
+	go sendMessage(ch1, "Message from channel 1")
+	go sendMessage(ch2, "Message from channel 2")
 
+	// Wait for the first message and exit
 	select {
-	case msg1 := <-ch1:
-		fmt.Println("Received:", msg1)
 	case msg2 := <-ch2:
 		fmt.Println("Received:", msg2)
+	case msg1 := <-ch1:
+		fmt.Println("Received:", msg1)
 	}
-}
+    }
+
+
